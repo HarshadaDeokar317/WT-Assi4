2f112ea96a22a76e53b8ec6ae4172cdc1a524637
HarshadaDeokar317 <46487473+HarshadaDeokar317@users.noreply.github.com>
2019-03-02 15:18:47 +0530

    i have added servlet assignment

---
+++ WebApplication1/src/java/NewServlet.java
@@ -0,0 +1,85 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/HTML");
+        String Username=request.getParameter("Uid");
+        String Password=request.getParameter("pwd");
+        PrintWriter pw=response.getWriter();
+        if((Username.equals("Harshada")&&Password.equals("Harshu123")))
+        {
+        pw.print("<html><head></head>");
+        pw.print("<body>");
+        pw.print("welcome "+Username);
+        pw.print("</body></html>");
+        pw.close();
+        
+       
+    }
+        else
+        {
+            RequestDispatcher rd=request.getRequestDispatcher("Login.html");
+            rd.forward(request, response);
+        }
+
+    }
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+   
+    }
+
+
+
